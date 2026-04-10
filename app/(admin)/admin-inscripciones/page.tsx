d142206dfb85e2388d8144cbae1cf2c798030062
Roxana <herrera.roxy23@gmail.com>
2026-04-09 20:06:44 -0600

    okey

--- app/(admin)/admin-inscripciones/page.tsx
+++ app/(admin)/admin-inscripciones/page.tsx
@@ -77,7 +77,7 @@ export default function AdminInscripcionesPage() {
     try {
 
       const res = await fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}/api/inscripciones/pendientes`
+        `${process.env.NEXT_PUBLIC_API_URL}/inscripciones/pendientes`
       );
 
       if (res.ok) {
